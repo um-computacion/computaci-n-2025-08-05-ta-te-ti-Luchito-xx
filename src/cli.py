e1829c4ce0235cb4f6aacfae4162493c55911829
Luchito-xx <luchoactis16@gmail.com>
2025-08-11 23:10:53 -0300

    agregar validación de entrada y modifico para implementar Jugador()

--- src/cli.py
+++ src/cli.py
@@ -1,22 +1,31 @@
 from tateti import Tateti
+from exception import PosOcupadaException, RangosException
 
+def mostrar_tablero(tab):
+    print("\n", tab[0],"\n", tab[1], "\n", tab[2], "\n")
 
 def main():
     print("Bienvenidos al Tateti")
     juego = Tateti()
+
     while True:
-        print("Tablero: ")
-        print(juego.tablero.contenedor)
-        print("Turno: ")
-        print(juego.turno)
+        mostrar_tablero(juego.tablero.contenedor)
+        jugador = juego.jugador()
+        print(f"\nTurno del jugador: {jugador.nombre} \nFicha: {jugador.ficha} \n")
+
         try:
-            fil = int(input("Ingrese fila"))
-            col = int(input("Ingrese col"))
-            juego.ocupar_una_de_las_casillas(fil, col)
+            fil = int(input("Ingrese fila (0-2): "))
+            col = int(input("Ingrese columna (0-2): "))
+            if not (0 <= fil <= 2 and 0 <= col <= 2):
+                print("\nPor favor ingresa numeros validos (0, 1 o 2)")
+                continue
+            juego.ocupar_casilla(fil, col)
+        except PosOcupadaException as e:
+            print(e)
         except Exception as e:
             print(e)
-
-
+            mostrar_tablero(juego.tablero.contenedor)
+            break
 
 if __name__ == '__main__':
     main()
