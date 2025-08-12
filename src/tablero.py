3ddbca43a0c2cf14fd39f148d17a04b659119a90
Luchito-xx <luchoactis16@gmail.com>
2025-08-11 23:12:38 -0300

    cambios minimos

--- src/tablero.py
+++ src/tablero.py
@@ -1,6 +1,4 @@
-class PosOcupadaException(Exception):
-    ...
-
+from exception import PosOcupadaException
 
 class Tablero:
     def __init__(self):
@@ -11,8 +9,8 @@ class Tablero:
         ]
 
     def poner_la_ficha(self, fil, col, ficha):
-        # ver si esta ocupado...
+        # ver si esta ocupado
         if self.contenedor[fil][col] == "":
             self.contenedor[fil][col] = ficha
         else:
-            raise PosOcupadaException("pos ocupada!")
+            raise PosOcupadaException("\n !!Posicion ocupada¡¡")
